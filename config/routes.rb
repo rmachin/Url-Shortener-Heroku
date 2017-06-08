46cb66ba2dd1ea1ca21e80cb3f9af55701f1ae97
Raphael Machin <rmachin75@gmail.com>
2017-06-08 13:21:27 -0400

    update route and scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/new' => 'sites#new'
+
+  post '/site' => 'sites#create'
+
+  get ':url_short' => 'sites#show'
+
+  root 'sites#new'
+
 end
